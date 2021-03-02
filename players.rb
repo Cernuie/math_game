2a96683491c5c5483e95a058a0f6e955f852ba50
Alexander Nguyen <cernvii@gmail.com>
2021-03-02 02:51:03 -0500

    first commit

---
+++ players.rb
@@ -0,0 +1,21 @@
+class Player
+  
+  attr_accessor :name, :score
+
+  def initialize(name)
+    @name = name
+    @score = 3
+  end
+
+  def decrease_score
+    @score -= 1
+  end
+
+  def alive?
+    @score > 0
+  end
+
+end
+
+p = Player.new('1')
+puts p.score
